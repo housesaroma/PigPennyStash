08804b74ea71e84106be3325ff3075a9bc01e080
housesaroma <yahakulikov@yandex.ru>
2025-03-06 23:18:29 +0500

    clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { TuiRoot } from "@taiga-ui/core";
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {TuiRoot} from "@taiga-ui/core";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   title = 'PigPennyStash';
 }
