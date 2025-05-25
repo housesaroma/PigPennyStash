0f485c3cf27d31fb8bd82a3a04ed2c84dcdd08cf
StepanMkr <152437585+StepanMkr@users.noreply.github.com>
2025-05-25 09:10:37 +0500

    listAnimation

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import {provideHttpClient} from "@angular/common/http";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ],
 });
