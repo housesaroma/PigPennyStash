2f724102dd5110a31a2e1bfa613ef6ddb16e60e1
housesaroma <yahakulikov@yandex.ru>
2025-05-28 22:37:18 +0500

    change contact types

--- src/app/interfaces/event.interface.ts
+++ src/app/interfaces/event.interface.ts
@@ -1,9 +1,10 @@
 import { Contact } from "../models/contact.model";
+import {UserContacts} from "../services/contacts/contacts.service";
 
 export interface IEvent {
   id : number,
   title : string,
-  members: Contact[],
+  members: UserContacts[],
   deadline: Date | null,
   totalAmount: number | null,
 }
